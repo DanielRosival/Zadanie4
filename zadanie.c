2f49e8477fcffc9a356b7be76c4f31ea917ce26e
DanielRosival <daniel.rosival@gmail.com>
2017-06-24 19:01:09 +0200

    correction

--- zadanie.c
+++ zadanie.c
@@ -7,6 +7,12 @@
 #define LOWL_OK 1
 #define LOWL_PROBLEM -1
 #define LOWL_FAIL -1
+#define WRONG_SIZE -1
+#define EMPTY_LIST -1
+#define FIND_PREV_FAILED -1
+#define LIST_EMPTY 1
+#define ONLY_BEG 2
+#define CREATION_FAILED 1
 
 typedef struct OWN{
     int data;
@@ -20,20 +26,20 @@ typedef struct {
 
 
 OWN *find_prev(LOWL *list){
-
+    
     OWN *prev,*temp;
+    
     prev = list->beg;
     temp = list->beg->next;
-
+    
     if (list ==  NULL || list->beg == NULL){
-        return 1;
-
+        return LIST_EMPTY;
     }
     else if(list->beg->next == NULL){
-        return 2;
-
+        return ONLY_BEG;
+        
     }
-    else{
+    else {
         while(temp != NULL){
             if(temp == list->cur){
                 return prev;
@@ -42,144 +48,86 @@ OWN *find_prev(LOWL *list){
                 temp = temp->next;
                 prev = prev->next;
             }
-
         }
-
-
     }
-
-
+    return FIND_PREV_FAILED;
 }
 
 LOWL *lowl_create_empty(void){
     LOWL *temp;
-
+    
     temp = malloc(sizeof(LOWL));
-
-    if ( temp == NULL)
-    {
-        printf("Creation failed!\n");
+    
+    if ( temp == NULL){
         return NULL;
     }
-
+    
     temp -> beg = NULL;
     temp -> cur = NULL;
-
-    return temp;
-}
-
-LOWL *lowl_create_random(unsigned int size){
-    LOWL *temp;
-
-    temp = lowl_create_empty();
-
-    if ( temp == NULL )
-    {
-        printf("Creation failed\n");
-        return NULL;
-    }
-
-
-    temp -> beg = (OWN*) malloc (sizeof(OWN));
-    temp -> cur = (OWN*) malloc (sizeof(OWN));
-
-    if (temp -> beg == NULL)
-    {
-        printf("Creation failed!\n");
-        return NULL;
-    }
-
-    if (temp -> cur == NULL)
-    {
-        printf("Creation failed!\n");
-        return NULL;
-    }
-
-    temp -> beg -> data = rand()%100 + 1;
-    temp -> beg -> next = temp -> cur;
-
-    for (int i = 0; i < size - 1; i++ )
-    {
-        ((temp -> cur)+i) -> data = rand()%100 + 1;
-        if (i < size - 2)
-        {
-            ((temp -> cur)+i) -> next = ((temp -> cur) + i + 1);
-        }
-        else
-        {
-            ((temp -> cur)+i) -> next = NULL;
-        }
-    }
-
+    
     return temp;
 }
 
 
 void lowl_print(LOWL *list) {
     OWN *temp;
-
+    
+    if ( list -> beg == NULL) {
+        return;
+    }
+    
     temp = list -> beg;
-
+    
     while (temp != NULL){
-        printf("%d\n", temp -> data);
-        temp = temp -> next;
+        if ( temp  == list -> cur ) {
+            printf("CUR -> ( %d )\n", temp -> data);
+            temp = temp -> next;
+        } else {
+            printf("%d\n", temp -> data);
+            temp = temp -> next;
+        }
     }
 }
 
 char lowl_cur_step_right(LOWL *list) {
-
+    
     if (list -> cur -> next == NULL) {
         return BOUNDARY_REACHED;
     }
     else {
         list -> cur = list -> cur -> next;
     }
-
+    
     return LOWL_SUCCESS;
 }
 
 char lowl_cur_step_left(LOWL *list) {
     OWN *prev;
-
+    
     prev = find_prev(list);
-
-    if ( prev == 2 || prev == 1) {
+    
+    if ( prev == ONLY_BEG || prev == LIST_EMPTY ) {
         return BOUNDARY_REACHED;
     }
     else {
         list -> cur = prev;
     }
-
+    
     return LOWL_SUCCESS;
 }
 
-OWN *lowl_insert_right(LOWL* list, int val) {
-    OWN *node = malloc(sizeof(OWN));
-    node->data = val;
-    node->next = NULL;
-    if(list->beg == NULL){
-        list->beg = node;
-        list->cur = node;
-        return node;
-    }
-    else if(list->cur->next == NULL){
-        list->cur->next = node;
-        list->cur = node;
-        return node;
-    }
-    else{
-        node->next = list->cur->next;
-        list->cur->next = node;
-        list->cur = node;
-        return node;
-    }
-}
 
 OWN *lowl_insert_left(LOWL* list, int val) {
     OWN *node = malloc(sizeof(OWN));
+    
+    if ( node == NULL ) {
+        return NULL;
+    }
+    
     OWN *prev = find_prev(list);
     node->data = val;
     node->next = NULL;
+    
     if(list->beg == NULL){
         list->beg = node;
         list->cur = node;
@@ -199,6 +147,70 @@ OWN *lowl_insert_left(LOWL* list, int val) {
     }
 }
 
+OWN *lowl_insert_right(LOWL* list, int val) {
+    OWN *node = malloc(sizeof(OWN));
+    
+    if ( node == NULL ) {
+        return NULL;
+    }
+    
+    node->data = val;
+    node->next = NULL;
+    
+    if(list->beg == NULL){
+        list->beg = node;
+        list->cur = node;
+        return node;
+    }
+    else if (list->cur->next == NULL) {
+        list->cur->next = node;
+        list->cur = node;
+        return node;
+    }
+    else{
+        node->next = list->cur->next;
+        list->cur->next = node;
+        list->cur = node;
+        return node;
+    }
+    
+    return NULL;
+}
+
+LOWL *lowl_create_random(unsigned int size){
+    LOWL *temp;
+    int i;
+    int random_data;
+    
+    if ( size <= 0) {
+        return WRONG_SIZE;
+    }
+    
+    
+    if ( size == 1 ) {
+        temp = lowl_create_empty();
+        if ( temp == NULL ){
+            return NULL;
+        }
+        random_data = rand()%100 + 1;
+        lowl_insert_right (temp, random_data);
+        return temp;
+    }
+    
+    temp = lowl_create_empty();
+    if ( temp == NULL ){
+        return NULL;
+    }
+    
+    temp -> cur = temp -> beg;
+    
+    for ( i = 0; i < size; i++ ) {
+        random_data = rand()%100 + 1;
+        lowl_insert_right (temp, random_data);
+    }
+    return temp;
+}
+
 
 void create_svg (FILE *f, int max_value, int h) {
     fprintf(f, "<html>\n");
@@ -215,10 +227,10 @@ char lowl_visualize_schema_svg(LOWL *list, char *filename) {
     FILE *f;
     OWN *temp;
     int counter = 0, max_value, i = 1;
-
+    
     temp = list -> beg;
     max_value = temp -> data;
-
+    
     while (temp != NULL){
         counter++;
         if ( max_value < temp -> data) {
@@ -226,18 +238,18 @@ char lowl_visualize_schema_svg(LOWL *list, char *filename) {
         }
         temp = temp -> next;
     }
-
+    
     f = fopen (filename, "w");
-
+    
     if (f == NULL ) {
         return LOWL_PROBLEM;
     }
-
+    
     create_svg (f, max_value*10, counter*10);
-
+    
     temp = list -> beg;
-
-
+    
+    
     while (temp != NULL){
         fprintf(f, "%d.\n", i);
         fprintf(f, "<svg width=\"%d\" height=\"%d\"> \n", max_value*10, 20);
@@ -249,53 +261,60 @@ char lowl_visualize_schema_svg(LOWL *list, char *filename) {
         temp = temp -> next;
         i++;
     }
-
+    
     finish_svg(f);
-
+    
     fclose(f);
-
+    
     return LOWL_OK;
-
+    
 }
 
 void lowl_destroy(LOWL *list){
-    list -> cur = list -> beg;
     struct OWN* next;
-
-    while ( (list -> cur) != NULL )
-    {
+    
+    list -> cur = list -> beg;
+    
+    while ( (list -> cur) != NULL ) {
         next = list -> cur -> next;
         free(list -> cur);
         list -> cur = next;
     }
-
+    
     list -> beg = NULL;
     list -> cur = NULL;
+    
+    free(list);
 }
 
 char lowl_delete(LOWL *list) {
     OWN  *prev;
+    
     prev = find_prev(list);
-    if (prev == 2){
-        lowl_destroy(list);
+    
+    if ( prev == ONLY_BEG ) {
+        list -> beg = NULL;
+        list -> cur = NULL;
         return LOWL_OK;
     }
-    else if(prev == 1){
+    else if ( prev == LIST_EMPTY ) {
         return LOWL_OK;
     }
-    else{
-        if(list->cur->next == NULL){
+    else {
+        if( list->cur->next == NULL){
             prev->next = NULL;
             list->cur = prev;
+            free ( prev -> next );
             return LOWL_OK;
         }
         else{
             prev->next = list->cur->next;
             list->cur = list->cur->next;
+            free ( prev -> next );
             return LOWL_OK;
         }
     }
-
+    
     return LOWL_FAIL;
 }
 
@@ -303,13 +322,19 @@ char lowl_delete(LOWL *list) {
 int main(int argc, char const *argv[])
 {
     LOWL *temp;
-
+    
     srand(time(NULL));
+    
 
-    temp = lowl_create_random(50);
+    temp = lowl_create_random(1);
+
+    if (temp == WRONG_SIZE) {
+        return CREATION_FAILED;
+    }
+    
     lowl_print(temp);
-
+    
     lowl_visualize_schema_svg( temp, "visualize.html");
-
+    
     return 0;
 }
